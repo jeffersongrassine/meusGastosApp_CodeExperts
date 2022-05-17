b38d0f88349a2990f5edb650ea84c4bb96b8b9e9
jeffersongrassine <jeffersongrassine@gmail.com>
2022-05-17 14:45:22 -0300

    First Commit

---
+++ database/migrations/2022_05_17_171824_create_expenses_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('expenses', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            
+            $table->string('description');
+            $table->integer('type');
+            $table->integer('amount'); //29,90 -> 2990 | 9,99 -> 999
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('expenses');
+    }
+};
